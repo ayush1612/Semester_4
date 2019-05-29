a3b6f366e12b719bc21df0003cbf0684bb9ee5d3
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2019-05-29 18:49:08 +0530

    Update 2a.asm

--- MP_Lab/2a.asm
+++ MP_Lab/2a.asm
@@ -1,8 +1,13 @@
+;Bubble sort on an array
+;Tips and Doubts
+
+
+
 assume cs:code,ds:data
 data segment
-x db 10h,05h,03h,15h,01h
+x db 10h,05h,03h,15h,01h    ;the array on which we are going to work
 
-n dw n-x
+n dw n-x                    ;n=5
 data ends
 
 code segment
@@ -10,34 +15,34 @@ start:
     mov ax,data
     mov ds,ax
 
-    mov bx,n
-    dec  bx
+    mov bx,n        ;this is to store the counter for the outer loop
+    dec  bx         ;decrementing since if we compare the bubble sort algorithm we will find the outer loop runs from 0 to n-1
 
 next_pass:
-    mov cx,bx
+    mov cx,bx       ; since the inner loop's iterations depend on the number outer value of the loop variable
 
-that_pass:
-    mov ah,00
+that_pass:  
+    mov ah,00       ; since in the next line we are working on si which is a 16 bit register
     lea si,x
 
 next_cmp:
-    mov al,[si]
-    cmp al,[si+1]
-    jle do_nothing
-    xchg al,[si+1]
-    mov [si],al
-    mov ah,1
+    mov al,[si]     ;storing the value of [si] in al
+    cmp al,[si+1]    ; comparing it with the next value   
+    jle do_nothing      ;if less than or equal to we don't need to do any change 
+    xchg al,[si+1]      ;otherwise we need to swap
+    mov [si],al             
+    mov ah,1            ;this checks whether the array is sorted
 
 do_nothing:
-    inc si
-    loop next_cmp
-    cmp ah,0
-    je finish
+    inc si              ; increment si 
+    loop next_cmp       ; go for the next comparison
+    cmp ah,0            ;if found 0 it means the array is already sorted now no more executions required
+    je finish           ;jump to finish if ah=0
 
-    dec bx
-    jnz next_pass
+    dec bx              ;otherwise decrement bx
+    jnz next_pass       ;let's go for next iteration
 finish:
-    mov ah,4ch
+    mov ah,4ch          ;exit
     int 21h
 code ends
 end start
